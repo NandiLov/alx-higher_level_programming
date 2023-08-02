6653b455c462389970435c9032821efce9f3fe3e
NandiLov <nandilamul@gmail.com>
2023-08-02 08:13:12 -0700

    0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,1 +1,6 @@
-document.querySelector('HEADER').style.color = '#FF0000';
+
+// Select the <header> element using document.querySelector
+const headerElement = document.querySelector('header');
+
+// Update the text color of the <header> element to red (#FF0000)
+headerElement.style.color = '#FF0000';
